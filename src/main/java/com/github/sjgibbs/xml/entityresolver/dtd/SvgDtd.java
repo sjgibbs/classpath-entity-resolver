c0effa78d2ebd7ab4aec893e1bf79be4a6474ed0
Simon Gibbs <simon.gibbs@ft.com>
2017-06-08 13:54:19 +0100

    Describe SVG entities as Java constants

--- src/main/java/com/github/sjgibbs/xml/entityresolver/dtd/SvgDtd.java
+++ src/main/java/com/github/sjgibbs/xml/entityresolver/dtd/SvgDtd.java
@@ -9,6 +9,59 @@ import com.github.sjgibbs.xml.entityresolver.ClasspathEntityMapping;
  */
 public class SvgDtd  {
 
-	public static ClasspathEntityMapping VERSION_1_1 = new ClasspathEntityMapping("-//W3C//DTD SVG 1.1//EN","http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd",SvgDtd.class.getClassLoader(),"svg11.dtd");
+	public static ClasspathEntityMapping ANIMATION_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Animation//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-animation.mod", "svg-animation.mod");
+	public static ClasspathEntityMapping ANIM_EVENTS_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Animation Events Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-animevents-attrib.mod", "svg-animevents-attrib.mod");
+	public static ClasspathEntityMapping BASIC_CLIP_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Basic Clip//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-basic-clip.mod", "svg-basic-clip.mod");
+	public static ClasspathEntityMapping BASIC_FILTER_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Basic Filter//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-basic-filter.mod", "svg-basic-filter.mod");
+	public static ClasspathEntityMapping BASIC_FONT_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Basic Font//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-basic-font.mod", "svg-basic-font.mod");
+	public static ClasspathEntityMapping BASIC_GRAPHICS_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Basic Graphics Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-basic-graphics-attrib.mod", "svg-basic-graphics-attrib.mod");
+	public static ClasspathEntityMapping BASIC_PAINT_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Basic Paint Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-basic-paint-attrib.mod", "svg-basic-paint-attrib.mod");
+	public static ClasspathEntityMapping BASIC_STRUCTURE_1_1 = map( "//W3C//ELEMENTS SVG 1.1 Basic Structure//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-basic-structure.mod", "svg-basic-structure.mod");
+	public static ClasspathEntityMapping BASIC_TEXT_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Basic Text//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-basic-text.mod", "svg-basic-text.mod");
+	public static ClasspathEntityMapping CLIP_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Clip//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-clip.mod", "svg-clip.mod");
+	public static ClasspathEntityMapping CONDITIONAL_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Conditional Processing//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-conditional.mod", "svg-conditional.mod");
+	public static ClasspathEntityMapping CONTAINER_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Container Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-container-attrib.mod", "svg-container-attrib.mod");
+	public static ClasspathEntityMapping CORE_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Core Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-core-attrib.mod", "svg-core-attrib.mod");
+	public static ClasspathEntityMapping CURSOR_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Cursor//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-cursor.mod", "svg-cursor.mod");
+	public static ClasspathEntityMapping DATATYPES_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Datatypes//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-datatypes.mod", "svg-datatypes.mod");
+	public static ClasspathEntityMapping DOC_EVENTS_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Document Events Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-docevents-attrib.mod", "svg-docevents-attrib.mod");
+	public static ClasspathEntityMapping EXTENSIBILITY_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Extensibility//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-extensibility.mod", "svg-extensibility.mod");
+	public static ClasspathEntityMapping EXT_RESOURCES_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 External Resources Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-extresources-attrib.mod", "svg-extresources-attrib.mod");
+	public static ClasspathEntityMapping FILTER_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Filter//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-filter.mod", "svg-filter.mod");
+	public static ClasspathEntityMapping FONT_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Font//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-font.mod", "svg-font.mod");
+	public static ClasspathEntityMapping FRAMEWORK_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Modular Framework//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-framework.mod", "svg-framework.mod");
+	public static ClasspathEntityMapping GRADIENT_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Gradient//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-gradient.mod", "svg-gradient.mod");
+	public static ClasspathEntityMapping GRAPH_EVENTS_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Graphical Element Events Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-graphevents-attrib.mod", "svg-graphevents-attrib.mod");
+	public static ClasspathEntityMapping GRAPHICS_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Graphics Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-graphics-attrib.mod", "svg-graphics-attrib.mod");
+	public static ClasspathEntityMapping HYPERLINK_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Hyperlinking//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-hyperlink.mod", "svg-hyperlink.mod");
+	public static ClasspathEntityMapping IMAGE_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Image//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-image.mod", "svg-image.mod");
+	public static ClasspathEntityMapping MARKER_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Marker//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-marker.mod", "svg-marker.mod");
+	public static ClasspathEntityMapping MASK_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Mask//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-mask.mod", "svg-mask.mod");
+	public static ClasspathEntityMapping OPACITY_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Paint Opacity Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-opacity-attrib.mod", "svg-opacity-attrib.mod");
+	public static ClasspathEntityMapping PAINT_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Paint Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-paint-attrib.mod", "svg-paint-attrib.mod");
+	public static ClasspathEntityMapping PATTERN_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Pattern//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-pattern.mod", "svg-pattern.mod");
+	public static ClasspathEntityMapping PROFILE_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Color Profile//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-profile.mod", "svg-profile.mod");
+	public static ClasspathEntityMapping QNAME_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Qualified Name//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-qname.mod", "svg-qname.mod");
+	public static ClasspathEntityMapping SCRIPT_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Scripting//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-script.mod", "svg-script.mod");
+	public static ClasspathEntityMapping SHAPE_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Shape//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-shape.mod", "svg-shape.mod");
+	public static ClasspathEntityMapping STRUCTURE_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Structure//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-structure.mod", "svg-structure.mod");
+	public static ClasspathEntityMapping STYLE_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Style//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-style.mod", "svg-style.mod");
+	public static ClasspathEntityMapping TEXT_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 Text//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-text.mod", "svg-text.mod");
+	public static ClasspathEntityMapping VIEW_1_1 = map( "-//W3C//ELEMENTS SVG 1.1 View//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-view.mod", "svg-view.mod");
+	public static ClasspathEntityMapping VIEWPORT_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Viewport Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-viewport-attrib.mod", "svg-viewport-attrib.mod");
+	public static ClasspathEntityMapping XLINK_ATTRIB_1_1 = map( "-//W3C//ENTITIES SVG 1.1 XLink Attribute//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg-xlink-attrib.mod", "svg-xlink-attrib.mod");
+	public static ClasspathEntityMapping ATTRIBS_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Attribute Collection//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11-attribs.mod", "svg11-attribs.mod");
+	public static ClasspathEntityMapping BASIC_ATTRIBS_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Basic Attribute Collection//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11-basic-attribs.mod", "svg11-basic-attribs.mod");
+	public static ClasspathEntityMapping BASIC_MODEL_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Basic Document Model//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11-basic-model.mod", "svg11-basic-model.mod");
+	public static ClasspathEntityMapping BASIC_1_1 = map( "-//W3C//DTD SVG 1.1 Basic//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11-basic.dtd", "svg11-basic.dtd");
+	public static ClasspathEntityMapping MODEL_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Document Model//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11-model.mod", "svg11-model.mod");
+	public static ClasspathEntityMapping TINY_ATTRIBS_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Tiny Attribute Collection//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11-tiny-attribs.mod", "svg11-tiny-attribs.mod");
+	public static ClasspathEntityMapping TINY_MODEL_1_1 = map( "-//W3C//ENTITIES SVG 1.1 Tiny Document Model//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11-tiny-model.mod", "svg11-tiny-model.mod");
+	public static ClasspathEntityMapping TINY_1_1 = map( "-//W3C//DTD SVG 1.1 Tiny//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11-tiny.dtd", "svg11-tiny.dtd");
+	public static ClasspathEntityMapping SVG_1_1 = map( "-//W3C//DTD SVG 1.1//EN", "https://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd", "svg11.dtd");
+
+	private static ClasspathEntityMapping map(String publicId, String systemId, String path) {
+		return new ClasspathEntityMapping(publicId, systemId,SvgDtd.class, path);
+	}
 
 }
